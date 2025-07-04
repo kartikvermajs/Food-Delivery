f7d85faba7c2fa7fda412e1fa9682b0c5c6a3580
MONKEY-D <118673486+MONKEY-D@users.noreply.github.com>
2025-07-04 17:51:01 +0530

    core updated

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,51 @@
-import express from "express"
-import cors from "cors"
-import {connectDB} from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
-import userRouter from "./routes/userRoute.js"
-import 'dotenv/config'
-import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/orderRoute.js"
-
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 // app config
-const app = express()
-const port = 4000
+const app = express();
+const port = 4000;
 
 // middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(
+  cors({
+    origin: "https://tomatofront-il1y.onrender.com",
+    credentials: true,
+  })
+);
 
 // db connection
 connectDB();
 
 // api endpoint
-app.use("/api/food",foodRouter)
-app.use("/images",express.static('uploads'))
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 // Verification endpoint
 app.get("/verify", (req, res) => {
-    const { success, orderId } = req.query;
+  const { success, orderId } = req.query;
 
-    // You can handle different response formats here
-    res.status(200).json({
-        success,
-        orderId,
-        message: success === 'true' ? 'Payment was successful!' : 'Payment failed!',
-    });
+  // You can handle different response formats here
+  res.status(200).json({
+    success,
+    orderId,
+    message: success === "true" ? "Payment was successful!" : "Payment failed!",
+  });
 });
 
-app.get("/",(req, res)=>{
-    res.send("API Working")
-})
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
 
-app.listen(port,()=>{
-    console.log(`Server Started on http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server Started on http://localhost:${port}`);
+});
